7349607c407af95c0194badc150eac529866043e
OmarAdel-arch <omaradelmax2002@gmail.com>
2026-02-26 14:25:06 +0200

    fix brands page prerender error

--- src/app/(page)/Categories/page.tsx
+++ src/app/(page)/Categories/page.tsx
@@ -12,7 +12,7 @@ export default async function categories() {
             <section className='mx-auto max-w-6xl py-25'>
                 <h2 className='font-bold text-2xl py-10'>Brands</h2>
                 <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4 lg:gap-6'>
-                    {categories.map((cats) =>
+                    {categories?.map((cats) =>
                         <div className=' shadow-md rounded-lg overflow-hidden mt-4' key={cats._id}>
                             <div className=' col-span-1 h-60 relative '>
                                 <Image fill src={cats.image} alt={cats.name}></Image>
--- src/app/(page)/brands/page.tsx
+++ src/app/(page)/brands/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 export default async function Brands() {
     const data: BrandDataI = await getAllBrands()
-    console.log(data.data);
+    const brands = data?.data || [];
 
 
     getUserToken()
@@ -15,7 +15,7 @@ export default async function Brands() {
             <section className='mx-auto max-w-6xl py-25'>
                 <h2 className='font-bold text-2xl py-10'>Brands</h2>
                 <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4 lg:gap-6'>
-                    {data.data.map((brand) =>
+                    {brands?.map((brand) =>
                         <div className=' shadow-md rounded-lg overflow-hidden mt-4' key={brand._id}>
                             <div className=' col-span-1 h-60 relative '>
                                 <Image fill src={brand.image} alt={brand.name}></Image>
